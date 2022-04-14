6146cd6b41e4b04dce95d9446703968d6511b757
DimaZap1990 <zapilenovD@gmail.com>
2022-04-14 21:40:34 +0300

    1st try

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  let f = Math.pow(b)-4*a*c
+  if (f<0){
+    return [];
+  }else if (f==0) {
+    let [d] = -b/(2*a)
+    return (d);
+  } else if (f>0) {
+    let dis=[g,h]
+    let g = (-b + Math.sqrt(d) )/(2*a)
+    let h = (-b - Math.sqrt(d) )/(2*a)
+    return (dis);
+  }
   return arr; // array
 }
 
