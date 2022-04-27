9d99fd1cf69199334e82e4562bd88ca879613939
DimaZap1990 <zapilenovD@gmail.com>
2022-04-27 20:54:52 +0300

    task 3 done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,8 +1,9 @@
+'use strict';
 function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
-
+  result = arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i]);
   return result; // boolean
 }
 
@@ -10,6 +11,9 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr= arr.filter(element => element > 0)
+  .filter(element=> element % 3 ===0)
+  . map(element=>element*10);
 
   return resultArr; // array
 }
