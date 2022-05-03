83496abefb4a77a3481a904a9983c08b9e81745b
DimaZap1990 <zapilenovD@gmail.com>
2022-05-03 12:06:02 +0300

    task 4 add

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
 function Student(name, gender, age) {
     // Ваш код
-
+    this.name= name;
+    this.gender= gender;
+    this.age= age;
 }
-
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject= subjectName;
 }
+Student.prototype.addMark= function (mark) {
+  if(this.marks=== undefined){
+     this.marks= [];
+     this.marks.push(mark);
+  } else{
+    this.marks.push(mark);
+  }
+}
+Student.prototype.addMarks=function (...letMarks){
+  if(this.marks=== undefined){
+    this.marks=[];
+  }
+  letMarks.forEach(mark=>this.marks.push(mark));
+}
+Student.prototype.getAverage= function (){
+  let sum=this.marks.reduce((a,b)=> a + b);
+  let average = sum / this.marks.length;
+  return average;
+}
+Student.prototype.exclude = function (reason){
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+
 
 // ваш код для остальных методов
